54f4bb86cd556d4674ab49397290eee457085851
Malik <hafizdaniyalshakeel@gmail.com>
2025-05-28 15:04:14 +0500

    fix: enhance adminLogin cookie settings for security

--- backend/controllers/adminLogin.js
+++ backend/controllers/adminLogin.js
@@ -11,20 +11,25 @@ const adminLogin = (req, res) => {
     password === process.env.ADMIN_PASSWORD
   ) {
     const token = jwt.sign({ email, password }, process.env.JWT_SECRET_KEY);
-    res.cookie("token", token);
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    });
     return res.json({ ok: true, message: "Admin authenticated" });
   }
 
   return res.json({ ok: false, message: "Invalid Credentials" });
 };
 
-const adminLogout = async (req, res) =>{
+const adminLogout = async (req, res) => {
   try {
     res.clearCookie("token");
-    return res.json({ok:true, message: "Admin logout"})
+    return res.json({ ok: true, message: "Admin logout" });
   } catch (error) {
     return res.json({ ok: false, message: error.message });
   }
-}
+};
 
 export { adminLogin, adminLogout };
